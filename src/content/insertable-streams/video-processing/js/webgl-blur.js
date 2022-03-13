afe3d3fc7d4a4bcaa252ed786a3b0205831e61f1
Ningxin Hu <ningxin.hu@intel.com>
2022-03-13 16:33:58 +0800

    Add resize stage

--- src/content/insertable-streams/video-processing/js/webgl-blur.js
+++ src/content/insertable-streams/video-processing/js/webgl-blur.js
@@ -19,20 +19,27 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
     this.canvas_ = null;
     /** @private {?WebGL2RenderingContext} */
     this.gl_ = null;
-    /** @private {?WebGLUniformLocation} location of inSampler */
-    this.sampler_ = null;
-    /** @private {?WebGLProgram} */
-    this.program_ = null;
-    /** @private {?WebGLTexture} input texture */
-    this.texture_ = null;
+    /** @private {string} */
+    this.debugPath_ = 'debug.pipeline.frameTransform_';
 
+    // Input texture
+    this.inputTexture_ = null;
+
+    // Resize program
+    this.resizeProgram_ = null;
+    this.resizeProgramSampler_ = null;
+
+    // Blur program
+    this.blurProgram_ = null;
+    this.blurProgramSampler_ = null;
+    this.texelSizeLocation_ = null;
+
+    // Resources for blur processing with size
+    this.size_ = {width: 513, height: 513};
     this.texture1_ = null;
     this.texture2_ = null;
     this.frameBuffer1_ = null;
     this.frameBuffer2_ = null;
-    /** @private {string} */
-    this.debugPath_ = 'debug.pipeline.frameTransform_';
-    this.size_ = {width: 480, height: 270};
   }
   /** @override */
   async init() {
@@ -47,18 +54,26 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
       return;
     }
     this.gl_ = gl;
-    const vertexShader = this.loadShader_(gl.VERTEX_SHADER,
-      `#version 300 es
-      precision mediump float;
+    const vertexShaderSrc = `#version 300 es
+      precision highp float;
       in vec2 a_position;
       in vec2 a_texCoord;
       out vec2 v_texCoord;
       void main() {
         gl_Position = vec4(a_position, 0.0, 1.0);
         v_texCoord = a_texCoord;
-      }`);
-    const fragmentShader = this.loadShader_(gl.FRAGMENT_SHADER,
-      `#version 300 es
+      }`;
+    const resizeFragmentShaderSrc = `#version 300 es
+      precision highp float;
+      uniform sampler2D u_inputFrame;
+      in vec2 v_texCoord;
+      out vec4 outColor;
+      void main() {
+        outColor = texture(u_inputFrame, v_texCoord);
+      }`;
+    this.resizeProgram_ = this.createProgram_(vertexShaderSrc, resizeFragmentShaderSrc);
+    this.resizeProgramSampler_ = gl.getUniformLocation(this.resizeProgram_, 'u_inputFrame');
+    const blurFragmentShaderSrc = `#version 300 es
       precision highp float;
       uniform sampler2D u_inputFrame;
       uniform vec2 u_texelSize;
@@ -77,55 +92,17 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
           frameColor += texture(u_inputFrame, texCoord) * weight[i];
         }
         outColor = vec4(frameColor.rgb + (1.0 - frameColor.a) * centerColor.rgb, 1.0);
-      }`);
-    if (!vertexShader || !fragmentShader) return;
-    // Create the program object
-    const programObject = gl.createProgram();
-    gl.attachShader(programObject, vertexShader);
-    gl.attachShader(programObject, fragmentShader);
-    // Link the program
-    gl.linkProgram(programObject);
-    // Check the link status
-    const linked = gl.getProgramParameter(programObject, gl.LINK_STATUS);
-    if (!linked) {
-      const infoLog = gl.getProgramInfoLog(programObject);
-      gl.deleteProgram(programObject);
-      throw new Error(`Error linking program:\n${infoLog}`);
-    }
-    gl.deleteShader(vertexShader);
-    gl.deleteShader(fragmentShader);
-    this.sampler_ = gl.getUniformLocation(programObject, 'u_inputFrame');
-    this.texelSizeLocation_ = gl.getUniformLocation(programObject, 'u_texelSize');
-    this.program_ = programObject;
-
-    this.attributeSetFloats_('a_position', 2, [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0]);
-    this.attributeSetFloats_('a_texCoord', 2, [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0]);
+      }`;
+    this.blurProgram_ = this.createProgram_(vertexShaderSrc, blurFragmentShaderSrc);
+    this.blurProgramSampler_ = gl.getUniformLocation(this.blurProgram_, 'u_inputFrame');
+    this.texelSizeLocation_ = gl.getUniformLocation(this.blurProgram_, 'u_texelSize');
   
     // Initialize input texture
-    this.texture_ = this.createTexture_();
-
+    this.inputTexture_ = this.createTexture_();
     this.texture1_ = this.createTexture_(this.size_.width, this.size_.height);
+    this.frameBuffer1_ = this.createFramebuffer_(this.texture1_);
     this.texture2_ = this.createTexture_(this.size_.width, this.size_.height);
-  
-    this.frameBuffer1_ = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer1_);
-    gl.framebufferTexture2D(
-      gl.FRAMEBUFFER,
-      gl.COLOR_ATTACHMENT0,
-      gl.TEXTURE_2D,
-      this.texture1_,
-      0
-    );
-  
-    this.frameBuffer2_ = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer2_)
-    gl.framebufferTexture2D(
-      gl.FRAMEBUFFER,
-      gl.COLOR_ATTACHMENT0,
-      gl.TEXTURE_2D,
-      this.texture2_,
-      0
-    );
+    this.frameBuffer2_ = this.createFramebuffer_(this.texture2_);
 
     console.log(
         '[WebGLTransform] WebGL initialized.', `${this.debugPath_}.canvas_ =`,
@@ -157,17 +134,18 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
 
   /**
    * Sets a floating point shader attribute to the values in arr.
+   * @param {WebGLProgram} program the WebGL program to set attributes
    * @param {string} attrName the name of the shader attribute to set
    * @param {number} vsize the number of components of the shader attribute's
    *   type
    * @param {!Array<number>} arr the values to set
    * @private
    */
-  attributeSetFloats_(attrName, vsize, arr) {
+  attributeSetFloats_(program, attrName, vsize, arr) {
     const gl = this.gl_;
     gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr), gl.STATIC_DRAW);
-    const attr = gl.getAttribLocation(this.program_, attrName);
+    const attr = gl.getAttribLocation(program, attrName);
     gl.enableVertexAttribArray(attr);
     gl.vertexAttribPointer(attr, vsize, gl.FLOAT, false, 0, 0);
   }
@@ -186,6 +164,47 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
     return texture;
   }
 
+  createFramebuffer_(texture) {
+    const gl = this.gl_;
+    const frameBuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER,
+      gl.COLOR_ATTACHMENT0,
+      gl.TEXTURE_2D,
+      texture,
+      0
+    );
+    return frameBuffer;
+  }
+
+  createProgram_(vertexShaderSrc, fragmentShaderSrc) {
+    const gl = this.gl_;
+    const vertexShader = this.loadShader_(gl.VERTEX_SHADER, vertexShaderSrc);
+    const fragmentShader = this.loadShader_(gl.FRAGMENT_SHADER, fragmentShaderSrc);
+    if (!vertexShader || !fragmentShader) {
+      throw new Error('Failed to load shader');
+    }
+    // Create the program object
+    const programObject = gl.createProgram();
+    gl.attachShader(programObject, vertexShader);
+    gl.attachShader(programObject, fragmentShader);
+    // Link the program
+    gl.linkProgram(programObject);
+    // Check the link status
+    const linked = gl.getProgramParameter(programObject, gl.LINK_STATUS);
+    if (!linked) {
+      const infoLog = gl.getProgramInfoLog(programObject);
+      gl.deleteProgram(programObject);
+      throw new Error(`Error linking program:\n${infoLog}`);
+    }
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+    this.attributeSetFloats_(programObject, 'a_position', 2, [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0]);
+    this.attributeSetFloats_(programObject, 'a_texCoord', 2, [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0]);
+    return programObject;
+  }
+
   /** @override */
   async transform(frame, controller) {
     const gl = this.gl_;
@@ -193,24 +212,32 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
       frame.close();
       return;
     }
-    const width = frame.displayWidth;
-    const height = frame.displayHeight;
-    if (this.canvas_.width !== width || this.canvas_.height !== height) {
-      this.canvas_.width = width;
-      this.canvas_.height = height;
-      gl.viewport(0, 0, width, height);
+    // Set output size to input size
+    if (this.canvas_.width !== frame.displayWidth || this.canvas_.height !== frame.displayHeight) {
+      this.canvas_.width = frame.displayWidth;
+      this.canvas_.height = frame.displayHeight;
     }
-    const timestamp = frame.timestamp;
-    const texelWidth = 1 / this.size_.width;
-    const texelHeight = 1 / this.size_.height;
+    // Upload frame to input texture
     gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, this.texture_);
+    gl.bindTexture(gl.TEXTURE_2D, this.inputTexture_);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, frame);
     frame.close();
-    gl.useProgram(this.program_);
-    gl.uniform1i(this.sampler_, 0);
-    
+
+    // Resize from input texture to texture2
+    gl.viewport(0, 0, this.size_.width, this.size_.height);
+    gl.useProgram(this.resizeProgram_);
+    gl.uniform1i(this.resizeProgramSampler_, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer2_);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+
+    // Blur
+    const texelWidth = 1 / this.size_.width;
+    const texelHeight = 1 / this.size_.height;
+    gl.useProgram(this.blurProgram_);
+    gl.activeTexture(gl.TEXTURE1)
+    gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
+    gl.uniform1i(this.blurProgramSampler_, 1);
     for (let i = 0; i < 3; i++) {
       gl.uniform2f(this.texelSizeLocation_, 0, texelHeight);
       gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer1_);
@@ -218,7 +245,7 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
 
       gl.activeTexture(gl.TEXTURE1)
       gl.bindTexture(gl.TEXTURE_2D, this.texture1_);
-      gl.uniform1i(this.sampler_, 1);
+      gl.uniform1i(this.blurProgramSampler_, 1);
 
       gl.uniform2f(this.texelSizeLocation_, texelWidth, 0);
       gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer2_);
@@ -227,11 +254,18 @@ class WebGLBlurTransform { // eslint-disable-line no-unused-vars
       gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
     }
 
+    // Resize from texture2 to canvas
+    gl.viewport(0, 0, this.canvas_.width, this.canvas_.height);
+    gl.useProgram(this.resizeProgram_);
+    gl.activeTexture(gl.TEXTURE1)
+    gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
+    gl.uniform1i(this.resizeProgramSampler_, 1);
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+  
+    // Create a video frame from canvas and enqueue it to controller
     // alpha: 'discard' is needed in order to send frames to a PeerConnection.
-    controller.enqueue(new VideoFrame(this.canvas_, {timestamp, alpha: 'discard'}));
+    controller.enqueue(new VideoFrame(this.canvas_, {timestamp: frame.timestamp, alpha: 'discard'}));
   }
 
   /** @override */
