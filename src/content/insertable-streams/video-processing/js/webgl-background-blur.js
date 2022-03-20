bb7aae891bef8e7d17b53c6da5f2e743c585e332
Ningxin Hu <ningxin.hu@intel.com>
2022-03-20 17:04:30 +0800

    Segment the input image directly in webgl background blur

--- src/content/insertable-streams/video-processing/js/webgl-background-blur.js
+++ src/content/insertable-streams/video-processing/js/webgl-background-blur.js
@@ -37,7 +37,8 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
     this.blurBackgroundLocation_ = null;
 
     // Resources for blur processing with size
-    this.segmentationSize_ = {width: 513, height: 513};
+    this.segmentationWidth_ = 513;
+    this.segmentationHeight_ = 513;
     this.texture1_ = null;
     this.texture2_ = null;
     this.frameBuffer1_ = null;
@@ -84,9 +85,7 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
     const blurFragmentShaderSrc = `#version 300 es
       precision highp float;
       uniform sampler2D u_inputFrame;
-      uniform sampler2D u_inputSegmentation;
       uniform vec2 u_texelSize;
-      uniform int u_blurBackground;
       in vec2 v_texCoord;
       out vec4 outColor;
       const float offset[5] = float[](0.0, 1.0, 2.0, 3.0, 4.0);
@@ -94,35 +93,44 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
 
       void main() {
         vec4 centerColor = texture(u_inputFrame, v_texCoord);
-        float label = u_blurBackground == 1 ? texture(u_inputSegmentation, vec2(v_texCoord[0], v_texCoord[1])).a : 0.0;
-        if (label == 0.0) {
-          vec4 frameColor = centerColor * weight[0];
-          for (int i = 1; i < 5; i++) {
-            vec2 offset = vec2(offset[i]) * u_texelSize;
-            vec2 texCoord = v_texCoord + offset;
-            frameColor += texture(u_inputFrame, texCoord) * weight[i];
-            texCoord = v_texCoord - offset;
-            frameColor += texture(u_inputFrame, texCoord) * weight[i];
-          }
-          outColor = vec4(frameColor.rgb + (1.0 - frameColor.a) * centerColor.rgb, 1.0);
-          // green screen for test
-          // outColor = vec4(0.0, 1.0, 0.0, 1.0);
-        } else {
-          outColor = centerColor;
+        vec4 frameColor = centerColor * weight[0];
+        for (int i = 1; i < 5; i++) {
+          vec2 offset = vec2(offset[i]) * u_texelSize;
+          vec2 texCoord = v_texCoord + offset;
+          frameColor += texture(u_inputFrame, texCoord) * weight[i];
+          texCoord = v_texCoord - offset;
+          frameColor += texture(u_inputFrame, texCoord) * weight[i];
         }
+        outColor = vec4(frameColor.rgb + (1.0 - frameColor.a) * centerColor.rgb, 1.0);
       }`;
     this.blurProgram_ = this.createProgram_(vertexShaderSrc, blurFragmentShaderSrc);
     this.blurProgramInputSampler_ = gl.getUniformLocation(this.blurProgram_, 'u_inputFrame');
-    this.blurProgramSegmentationSampler_ = gl.getUniformLocation(this.blurProgram_, 'u_inputSegmentation');
     this.texelSizeLocation_ = gl.getUniformLocation(this.blurProgram_, 'u_texelSize');
-    this.blurBackgroundLocation_ = gl.getUniformLocation(this.blurProgram_, 'u_blurBackground');
-  
-    // Initialize input texture
-    this.inputTexture_ = this.createTexture_();
-    this.texture1_ = this.createTexture_(this.segmentationSize_.width, this.segmentationSize_.height);
-    this.frameBuffer1_ = this.createFramebuffer_(this.texture1_);
-    this.texture2_ = this.createTexture_(this.segmentationSize_.width, this.segmentationSize_.height);
-    this.frameBuffer2_ = this.createFramebuffer_(this.texture2_);
+
+    const segmentFragmentShaderSrc = `#version 300 es
+      precision highp float;
+      uniform sampler2D u_inputFrame;
+      uniform sampler2D u_blurredInputFrame;
+      uniform sampler2D u_inputSegmentation;
+      in vec2 v_texCoord;
+      out vec4 outColor;
+
+      void main() {
+        vec4 inputColor = texture(u_inputFrame, v_texCoord);
+        vec4 blurredColor = texture(u_inputFrame, v_texCoord);
+        float label = texture(u_inputSegmentation, vec2(v_texCoord[0], 1.0 - v_texCoord[1])).a;
+        if (label == 0.0) {
+          outColor = texture(u_blurredInputFrame, vec2(v_texCoord[0], 1.0 - v_texCoord[1]));
+          // green screen for test
+          // outColor = vec4(0.0, 1.0, 0.0, 1.0);
+        } else {
+          outColor = texture(u_inputFrame, vec2(v_texCoord[0], 1.0 - v_texCoord[1]));
+        }
+      }`;
+    this.segmentProgram_ = this.createProgram_(vertexShaderSrc, segmentFragmentShaderSrc);
+    this.segmentProgramInputSampler_ = gl.getUniformLocation(this.segmentProgram_, 'u_inputFrame');
+    this.segmentProgramBlurredInputSampler_ = gl.getUniformLocation(this.segmentProgram_, 'u_blurredInputFrame');
+    this.segmentProgramSegmentationSampler_ = gl.getUniformLocation(this.segmentProgram_, 'u_inputSegmentation');
 
     // Load deeplab model
     // Initialize tf.js WebGL backend with this.gl_
@@ -147,6 +155,15 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
     console.log(
         '[WebGLBackgroundBlur] WebGL initialized.', `${this.debugPath_}.canvas_ =`,
         this.canvas_, `${this.debugPath_}.gl_ =`, this.gl_);
+  }
+
+  initTextures_(frameWidth, frameHeight) {
+    // Initialize input texture
+    this.inputTexture_ = this.createTexture_();
+    this.texture1_ = this.createTexture_(frameWidth, frameHeight);
+    this.frameBuffer1_ = this.createFramebuffer_(this.texture1_);
+    this.texture2_ = this.createTexture_(frameWidth, frameHeight);
+    this.frameBuffer2_ = this.createFramebuffer_(this.texture2_);
   }
 
   /**
@@ -253,19 +270,24 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
       return;
     }
     // Set output size to input size
-    if (this.canvas_.width !== frame.displayWidth || this.canvas_.height !== frame.displayHeight) {
-      this.canvas_.width = frame.displayWidth;
-      this.canvas_.height = frame.displayHeight;
+    const frameWidth = frame.displayWidth;
+    const frameHeight = frame.displayHeight;
+    if (this.canvas_.width !== frameWidth || this.canvas_.height !== frameHeight) {
+      this.canvas_.width = frameWidth;
+      this.canvas_.height = frameHeight;
+      this.initTextures_(frameWidth, frameHeight);
     }
 
     // Segmentation
-    const videoBitmap = await createImageBitmap(frame, {resizeWidth: this.segmentationSize_.width, resizeHeight: this.segmentationSize_.height});
-    const isBlurBackground = this.blurBackgroundCheckbox_.checked ? true : false;
+    
+    const isSegmentBackground = this.blurBackgroundCheckbox_.checked ? true : false;
     let resultTensor;
     let resultGPUData;
-    if (isBlurBackground) {
+    if (isSegmentBackground) {
+      const resizedVideoBitmap = await createImageBitmap(
+        frame, {resizeWidth: this.segmentationWidth_, resizeHeight: this.segmentationHeight_});
       resultTensor = tf.tidy(() => {
-        let inputTensor = tf.browser.fromPixels(videoBitmap);
+        let inputTensor = tf.browser.fromPixels(resizedVideoBitmap);
         const inputShape = inputTensor.shape;
         inputShape.unshift(1);
         inputTensor = inputTensor.reshape(inputShape);
@@ -275,38 +297,31 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
         return tf.stack([outputTensor, outputTensor, outputTensor, outputTensor], 3);
       });
       resultGPUData = resultTensor.dataToGPU();
+      resizedVideoBitmap.close();
     }
 
+    const videoBitmap = await createImageBitmap(frame);
     // Upload frame to input texture
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, this.inputTexture_);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, videoBitmap);
+    videoBitmap.close();
 
     // Blur
-    const texelWidth = 1 / this.segmentationSize_.width;
-    const texelHeight = 1 / this.segmentationSize_.height;
-    gl.viewport(0, 0, this.segmentationSize_.width, this.segmentationSize_.height);
-    gl.scissor(0, 0, this.segmentationSize_.width, this.segmentationSize_.height);
+    const texelWidth = 1 / frameWidth;
+    const texelHeight = 1 / frameHeight;
+    gl.viewport(0, 0, frameWidth, frameHeight);
+    gl.scissor(0, 0, frameWidth, frameHeight);
     gl.useProgram(this.blurProgram_);
-    if (isBlurBackground) {
-      gl.activeTexture(gl.TEXTURE1);
-      gl.bindTexture(gl.TEXTURE_2D, resultGPUData.texture);
-      gl.uniform1i(this.blurProgramSegmentationSampler_, 1);
-      gl.uniform1i(this.blurBackgroundLocation_, 1);
-    } else {
-      gl.uniform1i(this.blurBackgroundLocation_, 0);
-    }
-    gl.activeTexture(gl.TEXTURE2)
-    gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
     gl.uniform1i(this.blurProgramInputSampler_, 0);
     for (let i = 0; i < 3; i++) {
       gl.uniform2f(this.texelSizeLocation_, 0, texelHeight);
       gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer1_);
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 
-      gl.activeTexture(gl.TEXTURE2)
+      gl.activeTexture(gl.TEXTURE1)
       gl.bindTexture(gl.TEXTURE_2D, this.texture1_);
-      gl.uniform1i(this.blurProgramInputSampler_, 2);
+      gl.uniform1i(this.blurProgramInputSampler_, 1);
 
       gl.uniform2f(this.texelSizeLocation_, texelWidth, 0);
       gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer2_);
@@ -315,20 +330,30 @@ class WebGLBackgroundBlurTransform { // eslint-disable-line no-unused-vars
       gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
     }
 
-    if (isBlurBackground) {
+    gl.viewport(0, 0, frameWidth, frameHeight);
+    gl.scissor(0, 0, frameWidth, frameHeight);
+    if (isSegmentBackground) {  
+      gl.useProgram(this.segmentProgram_);
+      gl.uniform1i(this.segmentProgramInputSampler_, 0);
+      gl.activeTexture(gl.TEXTURE1)
+      gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
+      gl.uniform1i(this.segmentProgramBlurredInputSampler_, 1);
+      gl.activeTexture(gl.TEXTURE2);
+      gl.bindTexture(gl.TEXTURE_2D, resultGPUData.texture);
+      gl.uniform1i(this.segmentProgramSegmentationSampler_, 2);
+    } else {
+      gl.useProgram(this.resizeProgram_);
+      gl.activeTexture(gl.TEXTURE1)
+      gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
+      gl.uniform1i(this.resizeProgramInputSampler_, 1);
+    }
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    if (isSegmentBackground) {
       resultTensor.dispose();
       resultGPUData.tensorRef.dispose();
     }
-
-    // Resize from texture2 to canvas
-    gl.viewport(0, 0, this.canvas_.width, this.canvas_.height);
-    gl.scissor(0, 0, this.canvas_.width, this.canvas_.height);
-    gl.useProgram(this.resizeProgram_);
-    gl.activeTexture(gl.TEXTURE2)
-    gl.bindTexture(gl.TEXTURE_2D, this.texture2_);
-    gl.uniform1i(this.resizeProgramInputSampler_, 2);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
     gl.finish();
   
